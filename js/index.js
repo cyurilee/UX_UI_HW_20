7e0a991458a614c20bf4be7c5266c374ab9e6d43
Crystal Lee <cyurilee@gmail.com>
2021-11-19 22:20:57 -0500

    Final changes and updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 $('.dropdown-toggle').dropdown()
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > 20) {
+    $('#toTopBtn').fadeIn();
+    } else {
+    $('#toTopBtn').fadeOut();
+    }
+    });
+    
+    $('#toTopBtn').click(function() {
+    $("html, body").animate({
+    scrollTop: 0
+    }, 1000);
+    return false;
+    });
+    });
